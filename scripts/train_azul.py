d46b14c93e3882debe78fb047c8d678730d29d4e
tagore84 <alberto@pozoesteabn.es>
2025-05-18 10:31:01 +0200

    cuda

--- scripts/train_azul.py
+++ scripts/train_azul.py
@@ -55,7 +55,12 @@ def main():
             prev_checkpoint = default_latest
 
     # Select device
-    device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    elif torch.backends.mps.is_available():
+        device = torch.device('mps')
+    else:
+        device = torch.device('cpu')
     print(f"Using device: {device}")
 
     # Initialize environment and model
